12b3b4fb7e772ce9b969fe0aa7beeff6e38d7af2
vilanz <vs.lanzarini@gmail.com>
2022-07-03 19:48:46 -0300

    refactor: switch from webpack to vite

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
-import * as React from 'react';
-import { render } from 'react-dom';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import { getEmptyCellBoard, Board, updateCellBoard } from './game-logic';
 
 const randomizedBoard = getEmptyCellBoard().map((row) => row.map(() => Math.round(Math.random())));
 
-function App(): JSX.Element {
+function App() {
   const [board, setBoard] = React.useState<Board>(randomizedBoard);
 
   React.useEffect(() => {
@@ -17,17 +17,25 @@ function App(): JSX.Element {
   return (
     <div>
       {board.map((row, idx) => (
-        // eslint-disable-next-line react/no-array-index-key
         <div style={{ display: 'flex' }} key={idx}>
-          {
-            row.map((cell) => <div style={{ height: '50px', width: '50px', backgroundColor: cell ? 'orange' : 'black', transition: 'background-color 0.5s ease' }} />)
-          }
+          {row.map((cell) => (
+            <div
+              style={{
+                height: '50px',
+                width: '50px',
+                backgroundColor: cell ? 'orange' : 'black',
+                transition: 'background-color 0.5s ease',
+              }}
+            />
+          ))}
         </div>
       ))}
     </div>
   );
 }
 
-const slot = document.createElement('div');
-document.body.appendChild(slot);
-render(<App />, slot);
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
