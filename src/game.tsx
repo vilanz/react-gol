62c4d0a20c930b961d59af215e68c8709331de1c
vilanz <vs.lanzarini@gmail.com>
2022-07-03 20:22:04 -0300

    feat: pan and zoom (w/ a bigger board)

--- src/game.tsx
+++ src/game.tsx
@@ -1,28 +1,35 @@
 import React from 'react';
+import { TransformWrapper, TransformComponent } from 'react-zoom-pan-pinch';
 import { GameRow } from './board';
 import {
   getEmptyBoard, randomizeRow, Board, updateCellBoard,
 } from './game-logic';
 
+const BOARD_SIZE = 500;
+
 export function Game() {
   const [board, setBoard] = React.useState<Board>(
-    () => getEmptyBoard(60).map(randomizeRow),
+    () => getEmptyBoard(BOARD_SIZE).map(randomizeRow),
   );
 
   React.useEffect(() => {
     const refresh = setInterval(() => {
       setBoard((b) => updateCellBoard(b));
-    }, 300);
+    }, 3000);
     return () => clearInterval(refresh);
   }, []);
 
   return (
-    <div>
-      {board.map((row, idx) => (
-        // Row indexes are stable
-        // eslint-disable-next-line react/no-array-index-key
-        <GameRow row={row} key={idx} />
-      ))}
-    </div>
+    <TransformWrapper>
+      <TransformComponent>
+        <div>
+          {board.map((row, idx) => (
+            // Row indexes are stable
+            // eslint-disable-next-line react/no-array-index-key
+            <GameRow row={row} key={idx} />
+          ))}
+        </div>
+      </TransformComponent>
+    </TransformWrapper>
   );
 }
