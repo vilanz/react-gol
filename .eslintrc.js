eb336cf8afb92eb279ab8fdb5a407a1b305a403b
vilanz <vs.lanzarini@gmail.com>
2022-08-14 20:55:33 -0300

    config: fix linting setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,34 +2,29 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
+    jest: true,
   },
-  extends: [
-    'plugin:react/recommended',
-    'airbnb',
-  ],
-  parser: '@typescript-eslint/parser',
+  extends: ["plugin:react/recommended", "eslint:recommended"],
+  parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
-    ecmaVersion: 'latest',
-    sourceType: 'module',
+    ecmaVersion: "latest",
+    sourceType: "module",
   },
-  plugins: [
-    'react',
-    '@typescript-eslint',
-  ],
+  plugins: ["react", "@typescript-eslint"],
   rules: {
-    'import/extensions': 'off',
-    'react/jsx-filename-extension': ['error', { extensions: ['.ts', '.tsx'] }],
-    'import/prefer-default-export': 'off',
-    'no-plusplus': 'off',
-    'react/react-in-jsx-scope': 'off',
+    "import/extensions": "off",
+    "react/jsx-filename-extension": ["error", { extensions: [".ts", ".tsx"] }],
+    "import/prefer-default-export": "off",
+    "no-plusplus": "off",
+    "react/react-in-jsx-scope": "off",
   },
   settings: {
-    'import/resolver': {
+    "import/resolver": {
       node: {
-        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        extensions: [".js", ".jsx", ".ts", ".tsx"],
       },
     },
   },
