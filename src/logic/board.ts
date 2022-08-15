052b2582906e18b2d814daff4de9a2eab5e64db8
vilanz <vs.lanzarini@gmail.com>
2022-08-14 22:30:09 -0300

    fix: live neighbours counting

--- src/logic/board.ts
+++ src/logic/board.ts
@@ -25,44 +25,25 @@ export function drawPointInBoard(
   return newBoard;
 }
 
-function getLiveNeighbourCount(x: number, y: number, board: Board) {
-  let count = 0;
-
-  // iterate over all 8 neighbours' positions
-  for (let row = y - 1; row <= y + 1 && row > 0 && row < board.length; row++) {
-    for (
-      let col = x - 1;
-      col <= x + 1 && col > 0 && col < board[row].length;
-      col++
-    ) {
-      if (row === x && col === y) {
-        // ignore our own position, of course
-        continue;
-      }
-      const isNeighbourAlive = board[row][col];
-      if (isNeighbourAlive) {
-        count++;
-      }
-    }
-  }
-
-  return count;
-}
-
-function isCellNowAlive(x: number, y: number, board: Board): BoardCell {
-  const wasAlive = board[y][x];
-  const liveNeighbourCount = getLiveNeighbourCount(x, y, board);
-  return wasAlive
-    ? liveNeighbourCount === 2 || liveNeighbourCount === 3
-    : liveNeighbourCount === 3;
-}
-
 export function getNextGeneration(board: Board): Board {
   const newBoard: Board = [];
   for (let y = 0; y < board.length; y++) {
     newBoard[y] = [];
     for (let x = 0; x < board[y].length; x++) {
-      newBoard[y][x] = isCellNowAlive(x, y, board);
+      const wasAlive = board[y][x];
+      const liveNeighbourCount = [
+        [y - 1, x - 1],
+        [y - 1, x],
+        [y - 1, x + 1],
+        [y, x - 1],
+        [y, x + 1],
+        [y + 1, x - 1],
+        [y + 1, x],
+        [y + 1, x + 1],
+      ].filter(([ny, nx]) => board?.[ny]?.[nx]).length;
+      newBoard[y][x] = wasAlive
+        ? liveNeighbourCount === 2 || liveNeighbourCount === 3
+        : liveNeighbourCount === 3;
     }
   }
   return newBoard;
